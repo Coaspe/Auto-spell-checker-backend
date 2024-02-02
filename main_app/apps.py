5fdf249f33f7e78744050986a2d2ef83a92744e2
Coaspe <aspalt85@gmail.com>
2024-02-02 19:23:11 +0900

    refactor: Remove duplicated code

--- main_app/apps.py
+++ main_app/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
-from dotenv import load_dotenv
 
 
 class MainAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main_app"
-
-    def ready(self) -> None:
-        load_dotenv()
-        return super().ready()
